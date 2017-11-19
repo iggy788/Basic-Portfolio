884bafcb8d5ddac7c734d9183878bd6d9384e369
Mike <mike@Mike-Ignaczak-MacBook-Pro.local>
2017-11-19 15:29:25 -0600

    Done

--- testing_files/assets/javascript/dialogtech.js
+++
@@ -1,9 +0,0 @@
-<script type="text/javascript">
-    var _stk = "8ec91f7b8681922381f33b470379db97292e15d0";
-    (function(){
-        var a=document, b=a.createElement("script"); b.type="text/javascript";
-        b.async=!0; b.src=('https:'==document.location.protocol ? 'https://' :
-        'http://') + 'd31y97ze264gaa.cloudfront.net/assets/st/js/st.js';
-        a=a.getElementsByTagName("script")[0]; a.parentNode.insertBefore(b,a);
-    })();
-</script>
